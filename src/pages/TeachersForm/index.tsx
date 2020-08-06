4259804a9756ed39d5c28f5a77a3c2f09d915c86
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-08-06 01:19:57 -0300

    Select and textarea to form teacher

--- src/pages/TeachersForm/index.tsx
+++ src/pages/TeachersForm/index.tsx
@@ -3,6 +3,8 @@ import PageHeader from "../../components/PageHeader";
 import "./styles.css";
 import Input from "../../components/Input";
 import warningIcon from "../../assets/images/icons/warning.svg";
+import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
 
 export default function TeachersForms() {
 	return (
@@ -17,21 +19,45 @@ export default function TeachersForms() {
 					<Input name="name" label="Nome completo" />
 					<Input name="avatar" label="Avatar" />
 					<Input name="whatsapp" label="Whatsapp" />
+					<Textarea name="bio" label="Biografia" />
 				</fieldset>
 				<fieldset>
 					<legend>Sobre a aula</legend>
-					<Input name="subject" label="Materia" />
+					<Select
+						name="subject"
+						label="Materia"
+						options={[
+							{
+								value: "Artes",
+								label: "Artes",
+							},
+							{
+								value: "Artes1",
+								label: "Artes",
+							},
+							{
+								value: "Artes2",
+								label: "Artes",
+							},
+							{
+								value: "Artes3",
+								label: "Artes",
+							},
+							{
+								value: "Artes4",
+								label: "Artes",
+							},
+						]}
+					/>
 					<Input name="cost" label="Custo da sua hora por aula" />
 				</fieldset>
 				<footer>
 					<p>
-						<img src={warningIcon} alt="Aviso importante"/>
-						Importante <br/>
+						<img src={warningIcon} alt="Aviso importante" />
+						Importante <br />
 						Preencha todos os dados
 					</p>
-					<button type="button">
-						Salvar cadastro
-					</button>
+					<button type="button">Salvar cadastro</button>
 				</footer>
 			</main>
 		</div>
