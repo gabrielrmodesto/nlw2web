a2f51e3798d6e0d41857602a5fa2b87663222ffb
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-08-30 20:10:14 -0300

    Landing page bring connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logoIMG from "../../assets/images/logo.svg";
 import landingImage from "../../assets/images/landing.svg";
 import studyIcon from "../../assets/images/icons/study.svg";
@@ -6,8 +6,18 @@ import giveClasses from "../../assets/images/icons/give-classes.svg";
 import purpleHeart from "../../assets/images/icons/purple-heart.svg";
 import "./styles.css";
 import { Link } from "react-router-dom";
+import api from "../../services/api";
 
 function Landing() {
+	const [totalConnections, setTotalConnections] = useState(0);
+
+	useEffect(() => {
+		api.get("connections").then((response) => {
+			const { total } = response.data;
+
+			setTotalConnections(total);
+		});
+	}, []);
 	return (
 		<div id="page-landing">
 			<div id="page-landing-content" className="container">
@@ -31,7 +41,7 @@ function Landing() {
 					</Link>
 				</div>
 				<span className="total-connections">
-					Total de 200 conexoes ja realizadas{" "}
+					Total de {totalConnections} conexoes ja realizadas{" "}
 					<img src={purpleHeart} alt="Coracao roxo" />
 				</span>
 			</div>
