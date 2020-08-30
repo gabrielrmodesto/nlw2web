dc3c877dd9610d2dce35bc1b4c24891052f2f77a
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-08-30 18:56:20 -0300

    Routes in App Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./assets/styles/global.css";
-import Routes from "./routes";
+import App from "./App";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Routes />
+		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
